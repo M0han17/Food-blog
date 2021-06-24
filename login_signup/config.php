336102cf5223d8243a72e6fe2e2a9f1b65095bf2
Mohan <85407008+M0han17@users.noreply.github.com>
2021-06-24 15:06:24 +0530

    add login and signup requisites

---
+++ login_signup/config.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+$dbHost = 'sql112.epizy.com';
+$dbName = 'epiz_28954844_projectDb';
+$dbUsername = 'epiz_28954844';
+$dbPassword = 'st4M2oydCI';
+$dbc= mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName); 
+?>
